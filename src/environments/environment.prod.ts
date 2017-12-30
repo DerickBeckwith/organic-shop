97e94f02178797b9ad9e9920e609f871e47866b0
Derick Beckwith <derick.beckwith@gmail.com>
2017-12-30 17:30:44 -0500

    Add my Firebase project settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: 'AIzaSyCR7qhXrKGPWtpFT8F5qhkR7juLLzxH4A0',
+    authDomain: 'organic-shop-300c5.firebaseapp.com',
+    databaseURL: 'https://organic-shop-300c5.firebaseio.com',
+    projectId: 'organic-shop-300c5',
+    storageBucket: 'organic-shop-300c5.appspot.com',
+    messagingSenderId: '282779234958'
   }
 };
